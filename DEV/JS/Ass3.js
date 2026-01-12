033a2e9b98850b8c08e3c51b1e93a4995ba45264
Zain Saqib <xain.saqib@gmail.com>
2026-01-13 00:33:33 +0500

    Assignment 5 completed

--- DEV/JS/Ass3.js
+++ DEV/JS/Ass3.js
@@ -1,43 +1,47 @@
 // q1
-let arr=[1,2,3,4,5,6,2,3]
-let num =2
+let arr = [1, 2, 3, 4, 5, 6, 2, 3];
+let num = 2;
 console.log(arr);
-arr.splice(arr.indexOf(num),1);
+arr.splice(arr.indexOf(num), 1);
 console.log(arr);
 
 // q2
 
-num=1234567;
-let count =0;
-while(num>0){
+num = 1234567;
+let count = 0;
+while (num > 0) {
   count++;
-  num=num/10
-  num=Math.floor(num);
+  num = num / 10;
+  num = Math.floor(num);
 }
 console.log(count);
 
 // q3
-num=287152;
-let sum=0;
-while(num>0){
-  sum=sum+num%10
-  num=num/10
-  num=Math.floor(num);
+num = 287152;
+let sum = 0;
+while (num > 0) {
+  sum = sum + (num % 10);
+  num = num / 10;
+  num = Math.floor(num);
 }
 console.log(sum);
 
-
 // q4
-num=7;
-let prod=1;
-for(let i=num ;i<=0;i--)
-{
-  prod=prod*i;
+num = 7;
+let prod = 1;
+if (num == 0 || num == 1) {
+  console.log(1);
+} 
+else {
+  for (let i = num; i >= 1; i--) {
+    prod = prod * i;
+   
+  }
+  console.log(prod);
 }
-console.log(prod);
 
 // q5
 
-copy=arr;
+copy = arr;
 copy.sort();
-copy[copy.length-1];
+copy[copy.length - 1];
---
+++ DEV/JS/Ass4.js
@@ -0,0 +1,31 @@
+// q1
+
+let num= Math.floor(Math.random()*6)+1;
+console.log(num)
+
+// q2
+
+let car={
+  name:"maruti", 
+  model: "550x",
+  color:["red","blue","white"]
+}
+
+console.log(car.name);
+
+
+// q3
+let person={
+  name:"zain",
+  age:18,
+  city:"Karachi"
+}
+
+
+person.city= "NewYork"
+person.country="United States"
+console.log(person)
+
+
+
+
