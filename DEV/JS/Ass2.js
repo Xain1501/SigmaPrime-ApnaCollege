ef366fb89ef64d76586aa60a21bdc53059ffb7bc
Zain Saqib <xain.saqib@gmail.com>
2026-01-12 21:22:17 +0500

    Done assignment 2 java script

---
+++ DEV/JS/Ass2.js
@@ -0,0 +1,55 @@
+// q1 and 2
+let arr=[7,9,0,-2];
+let n=3;
+let t= arr.length
+
+
+console.log(arr.slice(0,n));
+console.log(arr.reverse().slice(0,n));
+arr.reverse()
+
+
+
+let str="";
+
+if(str)
+{
+  console.log(" the string is not empty ")
+}
+else if(str)
+{
+  console.log(" the string is empty ")
+}
+
+// q4
+let char='abcd';
+let f=2;
+
+if(char[f]==char[f].toLowerCase()){
+   console.log("Letter is in lower case");
+
+}
+
+else {
+
+   console.log("Letter is in upper case");
+
+}
+
+
+
+let spaces="   zain  is awesome  "
+console.log(spaces);
+spaces=spaces.trim();
+console.log(spaces);
+
+
+// q6
+
+
+let arr2=[7,9,0,-2];
+
+arr2.includes(15);
+
+arr2.includes(9);
+
